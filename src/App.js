e83a76b71fbe25da16ba03e8df7f046c9a2bfc02
Hoang Luong <hoang.luong@altsrc.net>
2020-08-07 14:24:13 +0700

    add aws deploy config

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React con xx
+          Hello AWS!
         </a>
       </header>
     </div>
